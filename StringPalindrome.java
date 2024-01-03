8ffd05e9fb97b852a9aff21a6deac283a96e2e2c
Aishwarya Lokhande <aisha29301@gmail.com>
2024-01-03 14:05:40 +0530

    check if the string is a palindrome

---
+++ StringPalindrome.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class StringPalindrome {
+    public static boolean isAlphanumeric(char ch) {
+		return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9');
+	}
+	public static boolean checkPalindrome(String str) {
+		int start = 0, end = str.length() - 1;
+
+		while (start <= end) {
+			while (start <= end && !isAlphanumeric(str.charAt(start)))
+				start++;
+
+			while (start <= end && !isAlphanumeric(str.charAt(end)))
+				end--;
+
+			if (start <= end && Character.toLowerCase(str.charAt(start)) != Character.toLowerCase(str.charAt(end)))
+				return false;
+
+			start++;
+			end--;
+		}
+
+		return true;
+	    
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0;i<t;i++) {
+            String str = sc.next();
+            if(checkPalindrome(str)){
+                System.out.println("Yes");
+            }else{
+                System.out.println("No");
+            }
+        }
+
+    }
+}
