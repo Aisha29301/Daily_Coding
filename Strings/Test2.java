e733e9703b50fbb8efa0e23912ed02b6269be87b
aishwaryapl29 <111597400+Aisha29301@users.noreply.github.com>
2025-07-23 17:28:22 +0530

    test questions

---
+++ Strings/Test2.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class Test2 {
+    public static void main(String[] args) {
+        String s1 = "Welcome To  India";
+        s1 = s1.trim();
+        String rev = "";
+        // String rev = new StringBuilder(s1).reverse().toString();
+        String[] words = s1.split(" ");
+        for(int i=words.length-1;i>=0;i--){
+            rev += new StringBuilder(words[i]).reverse().append(" ");
+        }
+        System.out.println(rev);
+    }
+}
